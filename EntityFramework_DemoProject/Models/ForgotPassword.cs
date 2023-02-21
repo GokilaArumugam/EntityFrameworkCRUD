c67e0ad3a63ad376a9d0452e1cbf9c131f3ca5b0
gokila arumugam <gokigokila97513@gmail.com>
2023-02-21 14:47:42 +0530

    forgot update

---
+++ EntityFramework_DemoProject/Models/ForgotPassword.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace EntityFramework_DemoProject.Models
+{
+    public class ForgotPassword
+    {
+        [Required, DataType(DataType.EmailAddress), DisplayName("Email Id")]
+        public string Email_ID { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
